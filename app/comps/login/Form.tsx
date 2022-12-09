8168d4cff8a30ca7af98ffa6b6fb37c0ecd64d35
Krzysiek Godyn <Verti1234.krzysiek@o2.pl>
2022-12-09 23:14:33 +0100

    login form

---
+++ app/comps/login/Form.tsx
@@ -0,0 +1,44 @@
+import styles from './form.module.css'
+
+
+export default function Form() {
+
+
+
+    return (
+            
+                <div className="border-gray-500 border-2 rounded-3xl  w-3/5 h-1/2  font-sans scale-110 bg-white text-black md:w-2/5 lg:w-1/3 xl:w-1/4 2xl:w-1/5 dark:bg-black dark:text-white">
+                        <h2 className="my-8 text-3xl font-normal flex  justify-center ">Zaloguj się</h2>
+                        <form action="#" className="mx-8 mt-16  ">
+                            {/* poprzednio w input login byl name="email" */}
+                            <div className=" mt-10 flex flex-col border-b-2 border-black dark:border-white">
+                                <label htmlFor="login" className="text-xl">Login</label>
+                                <input 
+                                    type="textarea" 
+                                    name="login" id="login"  
+                                    placeholder="example@gmail.com" 
+                                    className={styles.login + " bg-white dark:bg-black"}  
+                                />
+
+                            </div>
+                            <div className="my-5 flex flex-col border-b-2 border-black dark:border-white">
+                                <label htmlFor="password" className="text-xl">Hasło</label>
+                                <input 
+                                    type="password" 
+                                    name="password" 
+                                    id="password" 
+                                    placeholder="**********"  
+                                    className={styles.login + " bg-white dark:bg-black"}/>
+                            </div>
+                            <div className="flex justify-center mt-20 text-3xl font-light">
+                                <button className={styles.lineAnimation}>Dalej</button>
+                            </div>
+                        </form>
+                </div>
+                
+                
+                
+            
+        
+    )
+}
