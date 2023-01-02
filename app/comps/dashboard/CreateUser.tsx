5be69ab2fb52bc2fad2a3b189dea0cd0495217b9
Krzysiek Godyn <Verti1234.krzysiek@o2.pl>
2023-01-02 18:54:39 +0100

    a lot of work (I'm proud)

---
+++ app/comps/dashboard/CreateUser.tsx
@@ -0,0 +1,11 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+export default function CreateUser() {
+    <div>
+        <form>
+            <TextField id="standard-basic" label="email" variant="standard" />
+            <Button variant="outlined">Invite</Button>
+        </form>
+    </div>
+}
