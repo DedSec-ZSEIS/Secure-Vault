eeda3df1b186ddf67a2915be067f79813b902481
Krzysiek Godyn <Verti1234.krzysiek@o2.pl>
2023-01-06 02:20:53 +0100

    react hook form (imo better)

--- app/comps/login/Form.tsx
+++ app/comps/login/Form.tsx
@@ -7,17 +7,19 @@ import { useStateContext } from "../../contexts/ContextProvider"
 import login from '../../utils/login';
 import register from '../../utils/register';
 // import { useEffect } from 'react';
-
-
+import {useForm, Controller} from 'react-hook-form'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const PATH = process.env.NEXT_PUBLIC_PATH
 const APIPATH = process.env.NEXT_PUBLIC_APIPATH
 
 
 export default function Form({ name, email='', uat='' }: { name: string, email?: string, uat?: string }) {
+    
     const router = useRouter()
     const {userData, setUserData} = useStateContext()
-    const handleSubmit = async (e: any) => {
+    const Submit = async (e: any) => {
         e.preventDefault()
         if(name === 'login') {
             const data = await login()
@@ -46,7 +48,7 @@ export default function Form({ name, email='', uat='' }: { name: string, email?:
             }
 
         } else {
-                const name = document.getElementById('name') as HTMLInputElement
+                
                 const password = document.getElementById('password') as HTMLInputElement
                 const hashedPassword = sha512(password.value).toString()
                 const data = await register(email, hashedPassword, uat)
@@ -55,45 +57,85 @@ export default function Form({ name, email='', uat='' }: { name: string, email?:
         }
     }
 
+    interface FormInputs {
+        firstName: string;
+        lastName: string;
+        password: string;
+        hashedpassword: string;
+        email: string;
+
+    }
+    const { register,  control } = useForm<FormInputs>();
+    
 
     return (
         <div className={'wrapper shadow w-3/5 h-1/2 md:w-2/5 lg:w-1/3 xl:w-1/4 2xl:w-1/5 relative ' + styles.wrapper}>
-            <div className="border-gray-500 border-2 rounded-3xl w-full h-full font-sans scale-110 bg-white text-black dark:bg-black dark:text-white ">
+            <div className="border-gray-500 border-1 rounded-3xl w-full h-full font-sans scale-110 bg-white text-black dark:bg-black dark:text-white ">
                 <h2 className="my-8 text-3xl font-normal flex  justify-center relative">
-                    {name === 'login' ? 'Zaloguj się' : 'Stwórz hasło'}
+                    {name === 'login' ? 'Zaloguj się' : 'Zarejestruj się'}
                 </h2>
-                <form action="#" className="mx-8 mt-16  ">
-                    {/* poprzednio w input login byl name="email" */}
-                    {
-                        name === 'login' && <div className=" mt-10 flex flex-col border-b-2 border-black dark:border-white">
-                        <label htmlFor="login" className="text-xl">Login</label>
-                        <input 
-                            type="textarea" 
-                            name="email" id="email"  
-                            placeholder="example@gmail.com"
-                            className={styles.login + " bg-white dark:bg-black"}  
-                        />
 
+                {
+                    name === 'activate' ? (<form onSubmit={Submit} className="mx-8 mt-12 ">
+                    <div className="  mx-5 flex flex-col  gap-1 ">
+                        <Controller
+                            render={({  }) => <TextField {...register("firstName")} id="firstName"label="First Name" variant="standard"/>}
+                            name="firstName"
+                            control={control}
+                            rules={{ required: true }}
+                            defaultValue=""
+                        />
+                        <Controller
+                            render={({  }) => <TextField {...register("lastName")} id="lastname" label="Last Name" variant="standard"/>}
+                            name="lastName"
+                            control={control}
+                            rules={{ required: true }}
+                            defaultValue=""
+                        />
+                        <Controller
+                            render={({ }) => <TextField {...register("password")} id="password" label="Create password" variant="standard" type="password"/>}
+                            name="password"
+                            control={control}
+                            rules={{ required: true }}
+                            defaultValue=""
+                        />
+                        <Controller
+                            render={({  }) => <TextField {...register("hashedpassword")} id="hashedpassword" label="Confirm password" variant="standard" type="password"/>}
+                            name="hashedpassword"
+                            control={control}
+                            rules={{ required: true }}
+                            defaultValue=""
+                        />
+                        {/* <input type="submit" /> */}
+                        <Button variant="outlined" type="submit" className='mt-5'>Wyślij</Button>
                     </div>
-                    }
-                    <div className="my-5 flex flex-col border-b-2 border-black dark:border-white">
-                        <label htmlFor="password" className="text-xl">Hasło</label>
-                        <input 
-                            type="password" 
-                            name="password" 
-                            id="password" 
-                            placeholder="**********"
-                            className={styles.login + " bg-white dark:bg-black"}/>
-                    </div>
-                    <div className="flex justify-center mt-20 text-3xl font-light">
-                        <button 
-                            className={styles.lineAnimation} 
-                            onClick={e => handleSubmit(e)} 
-                            >
-                            Dalej
-                        </button>
-                    </div>
-                </form>
+                </form>)
+                    :
+                    (
+                    <form onSubmit={Submit} className="mx-8 mt-12 flex flex-col">
+                        <div className=" mt-5 mx-5 flex flex-col  gap-1 ">
+                            <Controller
+                                render={({ field }) => <TextField {...field} id="email"label="E-mail" variant="standard"/>}
+                                name="email"
+                                control={control}
+                                rules={{ required: true }}
+                                defaultValue=""
+                            />
+                            
+                            <Controller
+                                render={({ field }) => <TextField {...field} id="password" label="password" variant="standard" type="password"/>}
+                                name="password"
+                                control={control}
+                                rules={{ required: true }}
+                                defaultValue=""
+                            />
+                            
+                            {/* <input type="submit" /> */}
+                            <Button variant="outlined" type="submit" className='mt-5'>Wyślij</Button>
+                        </div>
+                    </form>
+                    )
+                }
             </div>
         </div>
     )
